fcc3b774dfc9b09e4f23bc2c85454df275233890
GregoireAntoine <60960340+GregoireAntoine@users.noreply.github.com>
2020-08-28 09:46:58 +0200

    email unique

--- toothy.sql
+++ toothy.sql
@@ -1,9 +1,10 @@
+
 -- phpMyAdmin SQL Dump
 -- version 4.9.5
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : localhost:3307
--- Généré le : ven. 28 août 2020 à 07:10
+-- Généré le : ven. 28 août 2020 à 08:46
 -- Version du serveur :  5.7.24
 -- Version de PHP : 7.4.1
 
@@ -48,12 +49,29 @@ CREATE TABLE `commande` (
 CREATE TABLE `login` (
   `Nom` varchar(255) NOT NULL,
   `Prenom` varchar(255) NOT NULL,
-  `mail` varchar(255) NOT NULL,
-  `mdp` varchar(255) NOT NULL
+  `mdp` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Déchargement des données de la table `login`
+--
+
+INSERT INTO `login` (`Nom`, `Prenom`, `mdp`, `email`) VALUES
+('a', 'a', 'a', 'a@a'),
+('antoine', 'gregoire', 'greg', 'gregoireantoine@gmail.com');
+
+--
+-- Index pour les tables déchargées
+--
+
+--
+-- Index pour la table `login`
+--
+ALTER TABLE `login`
+  ADD UNIQUE KEY `mailindex` (`email`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-
